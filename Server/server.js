0545614543882527180803051b227e2101d9602c
Miguel Elizondo <miguelelizondov@gmail.com>
2019-01-12 11:22:55 -0600

    post-function

--- Server/server.js
+++ Server/server.js
@@ -8,20 +8,41 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var reservations = [];
+var reservations = [
+  {
+    routeName: "d",
+    name: "",
+    phoneNumber: 0,
+    email: "",
+    uniqueID: 0
+  }
+];
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"));
   });
   
-  app.get("/reservation", function(req, res) {
+  app.get("/reserv", function(req, res) {
     res.sendFile(path.join(__dirname, "../reservation.html"));
   });
 
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../tables.html"));
-  });
- 
+ app.get("/tables", function(req,res){
+   res.sendFile(path.join(__dirname, "../tables.html"));
+ });
+
+
+ app.post("/api/reservations",function(req,res){
+   var newReservation = req.body;
+
+   newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+   console.log(newReservation);
+
+   reservations.push(newReservation);
+   res.json(reservations);
+
+ });
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
